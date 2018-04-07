e5370ec1d3f456c425b4ee92861b71d25af76a02
Dmitry Orlov <nphoxconnect@gmail.com>
2018-04-07 17:28:39 +0300

    add Repository

---
+++ TestDataGenerator.Data/Repository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDataGenerator.Data
+{
+    public class Repository : IRepository
+    {
+        public string GetRandomName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetRandomPatronymic()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetRandomSurename()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetRandomUniqLogin()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Init()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
