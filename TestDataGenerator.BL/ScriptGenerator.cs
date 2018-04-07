627c5f4633a324de8c74ab52a7a6659768783635
Dmitry Orlov <nphoxconnect@gmail.com>
2018-04-07 17:33:14 +0300

    add ScriptGenerator

---
+++ TestDataGenerator.BL/ScriptGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestDataGenerator.Data;
+
+namespace TestDataGenerator.BL
+{
+    public class ScriptGenerator : IScriptGenerator
+    {
+        public string CreateScript(int entityCount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserEntity GenerateUser()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetInsertLine()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetValueLine(UserEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
